7dd6821839c738f57a861ecd28b8fb9ceaaa402e
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-04-10 19:17:50 -0300

    Clase 06 - WF

--- Clase06 - Windows Form/EjercicioI02/Login/Index.cs
+++ Clase06 - Windows Form/EjercicioI02/Login/Index.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿//Ejercicio I02 - Registrate
+//Consigna
+//Crear un proyecto de tipo Windows Forms App y un proyecto de biblioteca de clases.
+
+//Agregar al proyecto de biblioteca de clases la clase Ingresante 
+//tal como describe el siguiente diagrama:
+
+//El constructor inicializará todos los atributos de la clase.
+//El método Mostrar expondrá todos los atributos de la clase al exterior, 
+//utilizar StringBuilder e interpolación de strings.
+//El proyecto de Windows Forms contará con los siguientes controles:
+
+//Un control de tipo GroupBox que contendrá dos controles de tipo TextBox para 
+//cargar el nombre y la dirección, y un NumericUpDown para cargar la edad 
+//del usuario con sus respectivos Label.
+//Un segundo control de tipo GroupBox que contendrá tres RadioButton que determinarán 
+//el género del usuario (masculino, femenino o no binario).
+//Un último control de tipo GroupBox contendrá tres controles de tipo CheckBox con los 
+//distintos cursos a elegir (C#, C++ o JavaScript). Se deben poder elegir todos los 
+//cursos, ninguno o algunos.
+//Un control de tipo ListBox que tendrá los paises para que el 
+//usuario escoja (Argentina, Chile o Uruguay).
+//Por último, un botón que al accionarse deberá mostrar toda la 
+//información del usuario registrado.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;

c960f601d3cc07e154de523c74b5223f727c3700
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-04-10 17:42:44 -0300

    Clase WF

---
+++ Clase06 - Windows Form/EjercicioI01/Saludador/Saludo.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Saludador
+{
+    partial class Saludo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.lblSoy = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSaludo.Location = new System.Drawing.Point(40, 19);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(65, 25);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.Text = "label1";
+            // 
+            // lblSoy
+            // 
+            this.lblSoy.AutoSize = true;
+            this.lblSoy.Location = new System.Drawing.Point(43, 66);
+            this.lblSoy.Name = "lblSoy";
+            this.lblSoy.Size = new System.Drawing.Size(38, 15);
+            this.lblSoy.TabIndex = 1;
+            this.lblSoy.Text = "label1";
+            // 
+            // Saludo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 171);
+            this.Controls.Add(this.lblSoy);
+            this.Controls.Add(this.lblSaludo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Saludo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Saludo";
+            this.Load += new System.EventHandler(this.Saludo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Label lblSoy;
+    }
+}
---
+++ Clase06 - Windows Form/EjercicioI01/Saludador/Saludo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saludador
+{
+    public partial class Saludo : Form
+    {
+        public string mensaje;
+        public string nombre;
+        public string apellido;
+
+        public Saludo(string nombre, string apellido)
+        {
+            InitializeComponent();
+            this.mensaje = "¡Hola, Windows Forms!";
+            this.nombre = nombre;
+            this.apellido = apellido;
+
+        }
+
+        private void Saludo_Load(object sender, EventArgs e)
+        {
+            this.lblSaludo.Text = mensaje;
+            this.lblSoy.Text = $"Soy {nombre} {apellido}";
+        }
+    }
+}

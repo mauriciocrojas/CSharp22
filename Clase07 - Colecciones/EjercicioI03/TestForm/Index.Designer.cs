be91d6d0a28f7853976d5535b574e5a4a5b1a18d
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-04-12 12:16:42 -0300

    Clase 07 - Colecciones

---
+++ Clase07 - Colecciones/EjercicioI03/TestForm/Index.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TestForm
+{
+    partial class Index
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchPalabras = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnMostrarPalabras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rchPalabras
+            // 
+            this.rchPalabras.Location = new System.Drawing.Point(21, 12);
+            this.rchPalabras.Name = "rchPalabras";
+            this.rchPalabras.Size = new System.Drawing.Size(284, 347);
+            this.rchPalabras.TabIndex = 0;
+            this.rchPalabras.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(318, 12);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(110, 50);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnMostrarPalabras
+            // 
+            this.btnMostrarPalabras.Location = new System.Drawing.Point(318, 79);
+            this.btnMostrarPalabras.Name = "btnMostrarPalabras";
+            this.btnMostrarPalabras.Size = new System.Drawing.Size(110, 49);
+            this.btnMostrarPalabras.TabIndex = 2;
+            this.btnMostrarPalabras.Text = "Mostrar Palabras";
+            this.btnMostrarPalabras.UseVisualStyleBackColor = true;
+            this.btnMostrarPalabras.Click += new System.EventHandler(this.btnMostrarPalabras_Click);
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 370);
+            this.Controls.Add(this.btnMostrarPalabras);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rchPalabras);
+            this.Name = "Index";
+            this.Text = "Contador Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchPalabras;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnMostrarPalabras;
+    }
+}
---
+++ Clase07 - Colecciones/EjercicioI03/TestForm/Index.cs
@@ -0,0 +1,88 @@
+﻿//Ejercicio I03 - ¡A contar palabras!
+//Consigna
+//Crear un proyecto de tipo Windows Forms App, con un RichTextBox y un botón Calcular.
+
+//El formulario debe tener el siguiente aspecto:
+
+//Utilizar diccionarios(Dictionary<string, int>) para realizar un contador de palabras.
+
+//Se deberá recorrer el texto ingresado palabra por palabra. Si se trata de una nueva palabra,
+//se la agregará al diccionario y se inicializará su contador en 1. Caso contrario, se deberá 
+//incrementar dicho contador.
+//Ordenar los resultados de forma descendente por cantidad de apariciones de cada palabra.
+//Al presionar el botón Calcular informar mediante un MessageBox el TOP 3 de palabras con más 
+//apariciones
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Index : Form
+    {
+        public Dictionary<string, int> diccionario;
+        public List<string> listaPalabras;
+        public int cont = 0;
+        public Index()
+        {
+            InitializeComponent();
+            diccionario = new Dictionary<string, int>();
+            listaPalabras = new List<string>();
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            contadorPalabras(this.rchPalabras.Text);
+        }
+
+        private void btnMostrarPalabras_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Mostrar());
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var palabra in diccionario)
+            {
+                sb.Append($"Palabra: {palabra.Key}, Código: {palabra.Value}\n");
+            }
+            return sb.ToString();
+        }
+
+        public bool ExisteEnDiccionario(string palabra)
+        {
+            foreach (var item in diccionario)
+            {
+                if (item.Key == palabra)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void contadorPalabras(string palabra)
+        {
+            listaPalabras.Add(palabra);
+            foreach (var texto in listaPalabras)
+            {
+                if (!diccionario.ContainsKey(palabra))
+                {
+                    diccionario.Add(texto, 1);
+
+                }
+                else
+                {
+                    diccionario[texto] += 1;
+
+                }
+            }
+        }
+    }
+}

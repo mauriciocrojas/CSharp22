dfe90ff0921acf318cc62b4b30a46576275f2f2e
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-05-25 21:47:22 -0300

    EjercicioI01 - TestUni

---
+++ SP/Clase11 - Test Unit, Met. Ext/EjercicioI01 - TestUni/CalculatorTest/CalculadoraTest.cs
@@ -0,0 +1,96 @@
+//Crear una calculadora de string con un método int Add (string numeros) y 
+//automatizar las pruebas unitarias para cada uno de los siguientes puntos.
+
+//SUGERENCIAS
+//Comience con el caso de prueba más simple de un string vacío y pase a uno y dos números
+//Recuerde resolver las cosas de la forma más sencilla posible
+//Recuerde refactorizar después de cada prueba aprobada.
+//IMPORTANTE
+//El objetivo es aprender a trabajar de forma incremental.
+
+//Hacé una tarea a la vez y tratá de no seguir leyendo los siguientes puntos.
+
+//Parte I
+//El método Add debe poder tomar hasta dos números separados por comas, y devolverá su suma.
+
+//Por ejemplo "" o "1" o "1,2" como entradas (para un string vacío devolverá 0).
+
+//Parte II
+//Permitir que el método Add maneje una cantidad desconocida de números.
+
+//Parte III
+//Permita que el método Add admita salto de línea entre números (en lugar de comas).
+
+//La siguiente entrada está bien: "1 \ n2,3"(será igual a 6).
+//La siguiente entrada NO está bien: "1, \ n"(no es necesario probarlo, solo se aclara)
+//Parte IV
+//Permitir que el método Add soporte diferentes delimitadores.
+
+//Para cambiar un delimitador, el comienzo del string contendrá dos barras, debe verse así:
+
+//// [delimitador] \ n [números ...]
+
+//Por ejemplo: "//; \ n1; 2" debe devolver tres donde el valor predeterminado delimitador es ';'.
+
+//Las barras son opcionales. Todos los escenarios existentes aún deben ser compatibles.
+
+//Parte V
+//Si Add recibe un número negativo arrojará una excepción NegativoNoPermitidoException que en su 
+//mensaje contendrá el negativo que se recibió.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void Add_ProbarSumaDeString()
+        {
+            // Arrange - Preparar el caso de prueba
+            string numeros = "1,2";
+            Calculadora calculadora = new Calculadora();
+            int resultadoEsperado = 3;
+
+            // Act - Invocar al método a probar
+            int resultado = calculadora.Add(numeros);
+
+            // Assert - Verifico que el resultado sea el esperado.
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        public void Add_ProbarSumaDeStringConSaltoDeLinea()
+        {
+            // Arrange - Preparar el caso de prueba
+            string numeros = @"1 \ n2,3";
+            Calculadora calculadora = new Calculadora();
+            int resultadoEsperado = 6;
+
+            // Act - Invocar al método a probar
+            int resultado = calculadora.AddConSaltoYVacio(numeros);
+
+            // Assert - Verifico que el resultado sea el esperado.
+            Assert.AreEqual(resultadoEsperado, resultado);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NegativoNoPermitidoException))]
+        public void Add_ProbarNegativos()
+        {
+            // Arrange - Preparar el caso de prueba
+            string numeros = "-50";
+            Calculadora calculadora = new Calculadora();
+
+            // Act - Invocar al método a probar
+            int resultado = calculadora.AddNegativo(numeros);
+
+            //Assert
+            //Se espera que lance la excepción: NegativoNoPermitidoException
+
+
+        }
+    }
+}
---
+++ SP/Clase11 - Test Unit, Met. Ext/EjercicioI01 - TestUni/TestPorConsola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Entidades;
+
+namespace TestPorConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.WriteLine(calculadora.Add("1,2"));
+            Console.WriteLine(calculadora.AddConSaltoYVacio(@"1 \ n2,3"));
+
+            //Probamos la excepción
+            try
+            {
+
+            calculadora.AddNegativo("-50");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

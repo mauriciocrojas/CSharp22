07018c74a869bbfffaf4e0f1b9a96c2b02b0fa8b
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-05-30 12:27:02 -0300

    EjercicioI01 (parte 1) - Interfaces

---
+++ SP/Clase13 - Interfaces/EjercicioI01/Entidades/Boligrafo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Boligrafo : IAcciones
+    {
+
+        private ConsoleColor colorTinta;
+        private float tinta;
+
+        public Boligrafo(ConsoleColor colorTinta, float tinta)
+        {
+            this.Color = colorTinta;
+            this.UnidadesDeEscritura = tinta;
+        }
+
+        public ConsoleColor Color
+        {
+            get => this.colorTinta;
+            set => this.colorTinta = value;
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get => this.tinta;
+            set => this.tinta = value;
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+            //for (int i = 0; i < texto.Length; i++)
+            //{
+            //    this.UnidadesDeEscritura -= 0.3F;
+            //}
+
+            this.UnidadesDeEscritura -= texto.Length * 0.3f;
+
+            return new EscrituraWrapper(Color, texto);
+        }
+
+        public bool Recargar(int unidades)
+        {
+
+            this.UnidadesDeEscritura += unidades;
+            return true;
+
+        }
+
+        public override string ToString()
+        {
+            string cadena = $"Bolígrafo color {Color}, nivel de tinta {UnidadesDeEscritura}";
+            return cadena;
+        }
+    }
+}
---
+++ SP/Clase13 - Interfaces/EjercicioI01/Entidades/Lapiz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Lapiz : IAcciones
+    {
+        private float tamanioMina;
+
+        public Lapiz(int unidades)
+        {
+            ((IAcciones)this).UnidadesDeEscritura = unidades;
+        }
+
+        ConsoleColor IAcciones.Color
+        {
+            get => ConsoleColor.Gray;
+            set => throw new NotImplementedException();
+        }
+        float IAcciones.UnidadesDeEscritura
+        {
+            get => this.tamanioMina;
+            set => this.tamanioMina = value;
+        }
+
+        EscrituraWrapper IAcciones.Escribir(string texto)
+        {
+            //for (int i = 0; i < texto.Length; i++)
+            //{
+            //    this.tamanioMina -= 0.1F;
+            //}
+
+            ((IAcciones)this).UnidadesDeEscritura -= texto.Length * 0.1f;
+
+            return new EscrituraWrapper(((IAcciones)this).Color, texto);
+
+        }
+
+        bool IAcciones.Recargar(int unidades)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            string cadena = $"Bolígrafo color {((IAcciones)this).Color}, nivel de tinta {((IAcciones)this).UnidadesDeEscritura}";
+            return cadena;
+        }
+    }
+}
